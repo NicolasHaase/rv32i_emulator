b9fbedf1f47ccdd336ac1b8646f00757861dcc10
NicolasHaase <nicolasjimeneshaase@gmail.com>
2025-11-06 17:54:30 -0300

    comentarios

--- main.cpp
+++ main.cpp
@@ -20,13 +20,12 @@ void type_identifier(int x) {
     if (respbin == bitset<SIZE>(J_type_instruction)) cout << "Type J" << endl;
 }
 
-/*num_teste = 0b00000001111100000110111001110011/
-
-/** R-Type Bit division = |1111111|11111|11111|111|11111|
-    I-Type Bit division = |111111111111|11111|111|11111|
-    B-Type Bit division = |11111|11|11111|11111|111|11111|
-    U-Type Bit division = |11111111111111111111|11111|
-    J-Type Bit division = |11111|11|11111|11111|111|11111|
+/** num_teste = 0b00000001111100000110111001110011
+    R-Type Bit division = func7 = 1111111|rs2 = 11111|rs1 = 11111|func3 = 111|rd = 11111|opcode0110011
+    I-Type Bit division = imm[11:0] = 111111111111|rs1 = 11111|func 3 = 111|rd = 11111|opcode = 0010011
+    B-Type Bit division =
+    U-Type Bit division =
+    J-Type Bit division =
 */
 
 void R_division(int x) {
@@ -57,9 +56,83 @@ void R_division(int x) {
         cout << "Instruction: Unknown R-Type" << endl;
 }
 
+/**
+INSTRUCTIONS - TIPO I
+
+shamt
+SLLI - 001 (00000)
+SRLI - 101 (00000)
+SRAI - 101 (01000)
+
+ADDI - 000
+XORI - 100
+ORI - 110
+ANDI - 111
+SLTI - 010
+SLTIU - 011
+
+JALR - 000 (offset ao invés de imm) (opcode - 1100111)
+FENCE - 000 (opcode - 0001111)
+FENCE.I - 001 (opcode - 0001111)
+ECALL - 000 (opcode - 1110011)
+EBREAK - 000 (opcode - 1110011) (rs2[24:20] = 00001)
+*/
+
+void I_division(int x) {
+    int rd    = (x >> 7)  & 0b11111;// ele pega o num direto e não compara com o num inteiro
+    int funct3 = (x >> 12) & 0b111;
+    int rs1   = (x >> 15) & 0b11111;
+    int imm   = (x >> 20) & 0b111111111111;
+
+    cout << "imm:    " << bitset<12>(imm)    << " (" << imm << ")" << endl;
+    cout << "rs1:    " << bitset<5>(rs1)    << " (" << rs1 << ")" << endl;
+    cout << "funct3: " << bitset<3>(funct3) << " (" << funct3 << ")" << endl;
+    cout << "rd:     " << bitset<5>(rd)     << " (" << rd << ")" << endl;
+
+    // Identificação da instrução específica
+    if (funct3 == 0b000 && funct7 == 0b0000000)
+        cout << "Instruction: ADD" << endl;
+    else if (funct3 == 0b000 && funct7 == 0b0100000)
+        cout << "Instruction: SUB" << endl;
+    else if (funct3 == 0b111)
+        cout << "Instruction: AND" << endl;
+    else if (funct3 == 0b110)
+        cout << "Instruction: OR" << endl;
+    else if (funct3 == 0b100)
+        cout << "Instruction: XOR" << endl;
+    else
+        cout << "Instruction: Unknown I-Type" << endl;
+}
+
+/**
+INSTRUCTIONS - TIPO B
+
+BEQ - 000
+BNE - 001
+BLT - 100
+BGE - 101
+BLTU - 110
+BGEU - 111
+*/
+
+/**
+INSTRUCTIONS - TIPO U
+
+LUI - só tem imm, rd e opcode
+AUIPC - só tem imm, rd e opcode
+*/
+
+/**
+INSTRUCTIONS - TIPO J
+
+JAU - tem 4 imm diferentes, rd e opcode
+*/
+
+
 int main() {
-    int num1 = 0b00000001111100000110111001110011, num2 = 0b001000010011, num3 = 0b000111100011, num4 = 0b100000110111, num5 = 0b011101101111;
-   
+    int num1 = 0b00000001111100000110111001110011, num2 = 0b00000000000100001000000010010011, num3 = 0b000111100011, num4 = 0b100000110111, num5 = 0b011101101111;
+
+    cout << "socorro\n";
     type_identifier(num2);
    
     R_division(num2);
